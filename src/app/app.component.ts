b76ff4d5b3819fc9c0f26ff85730986d27b32ed0
alejandrofernandez2303 <alexchiquitin2303@gmail.com>
2022-02-28 23:24:12 +0100

    delocos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent 
+{
+  title = 'hola a esta primera clase';
+  name = 'alex';
+  rockBandName = '';
+
+  rockBands = ['acdc', 'heroes del silencio', 'mago'];
+
+  public changeTitle()
+  {
+    this.title = 'nuevo titulooo';
+  }
+
+  public addRockBand()
+  {
+    this.rockBands.push(this.rockBandName);
+    this.rockBandName = '';
+  }
 }
